8a8a716951fb578c2705954b8e267b52ba57a843
obivankanoobi <artium1@gmail.com>
2018-07-31 12:03:44 +0300

    changed the servers path in /routes/config from numeric to  as from some reason it didnt sent the activation mail otherwise

--- server/routes/config.js
+++ server/routes/config.js
@@ -6,8 +6,8 @@ let config = {
     smtp:"smtp.domain.com", // i.e 'smtp.domain.com'
     mongoUsername:"<mongo username>", // if you have your db
     mongoPw:"<mongo pw>", // if you have your db
-    server:"http://10.0.0.11:3010", // your frontend server
-    frontEndServer:"http://10.0.0.11:3000" // your front end server
+    server:"http://localhost:3010", // your frontend server
+    frontEndServer:"http://localhost:3000" // your front end server
 };
 
 
